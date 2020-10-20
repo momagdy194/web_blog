d97b4451bba6c90fdfd46c7ef323cbf782514c9a
momagdy <momagdy194@gmail.com>
2020-10-20 23:16:05 +0300

    add about page

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -10,5 +10,5 @@ class Post(models.Model):
     post_update = models.DateField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):  
+    def __str__(self):
         return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,29 +7,31 @@ posts = [
         'post_date': '15-3-2019',
         'auther': 'mohamed magdy'
     },
-        {
+    {
         'title': 'المدونه الاولي ',
         'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
         'post_date': '15-3-2019',
         'auther': 'mohamed magdy'
     },
-        {
+    {
         'title': 'المدونه الاولي ',
         'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
         'post_date': '15-3-2019',
         'auther': 'mohamed magdy'
     },
-        {
+    {
         'title': 'المدونه الاولي ',
         'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
         'post_date': '15-3-2019',
         'auther': 'mohamed magdy'
     },
-       
+
 ]
 
+
 def about(request):
-    return render(request,'blog/about.html',{"title":"عني"})
+    return render(request, 'blog/about.html', {"title": "عني"})
+
 
 def home(request):
     context = {
@@ -38,4 +40,3 @@ def home(request):
     }
 
     return render(request, 'blog/index.html', context)
-
