86695d77374ff1757b3056d074cefc4526d77959
momagdy <momagdy194@gmail.com>
2020-10-20 23:05:34 +0300

    add who iam

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    postDate = models.DateField(default=timezone.now())
+    post_update = models.DateField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):  
+        return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'المدونه الاولي ',
+        'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
+        'post_date': '15-3-2019',
+        'auther': 'mohamed magdy'
+    },
+        {
+        'title': 'المدونه الاولي ',
+        'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
+        'post_date': '15-3-2019',
+        'auther': 'mohamed magdy'
+    },
+        {
+        'title': 'المدونه الاولي ',
+        'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
+        'post_date': '15-3-2019',
+        'auther': 'mohamed magdy'
+    },
+        {
+        'title': 'المدونه الاولي ',
+        'content': 'قد حدث في الفتره الاخيره الكثير من الموارد الشيقه و الكثيره التي ادت الي حدوث الكثير من المواضيع الشيقه ',
+        'post_date': '15-3-2019',
+        'auther': 'mohamed magdy'
+    },
+       
+]
+
+def about(request):
+    return render(request,'blog/about.html',{"title":"عني"})
 
 def home(request):
-    return render(request, 'blog/index.html', {'title': 'Home'})
+    context = {
+        'title': 'مدونة| الصفحه الرئيسيه',
+        'posts': posts
+    }
+
+    return render(request, 'blog/index.html', context)
+
